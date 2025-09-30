d9bc028c27e058e2c1bae900993e16b7566a90db
raslenraslen <raslenm39@gmail.com>
2025-09-30 05:08:43 +0000

    xd

--- ansible-install/steps.sh
+++ ansible-install/steps.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-
+# Ajouter le PPA d'Ansible
 sudo apt-add-repository ppa:ansible/ansible -y
-ls
 
+# Mettre à jour les informations des paquets
 sudo apt update
 
 # Installer Ansible
@@ -11,5 +11,5 @@ sudo apt install ansible -y
 echo "raslen ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
 
 
-
+# Vérifier l'installation
 ansible --version
